0723ddd243f37ec8695f375440b880dd9bd767b3
CookieDE <46023076+CookieDE@users.noreply.github.com>
2018-12-26 11:17:51 +0100

    Update de.lua
    Übersetzung fertig!

--- [esx]/esx_billing/locales/de.lua
+++ [esx]/esx_billing/locales/de.lua
@@ -1,10 +1,10 @@
 Locales['de'] = {
-  ['invoices'] = 'rechnungen',
+  ['invoices'] = 'Rechnungen',
   ['invoices_item'] = '$%s',
   ['received_invoice'] = 'you hast eine Rechnung ~r~erhlaten~s~',
   ['paid_invoice'] = 'du ~g~bezahlst~s~ eine Rechnung von ~r~$%s~s~',
   ['received_payment'] = 'du ~g~erhältst~s~ eine Zahlung von ~r~$%s~s~',
   ['player_not_online'] = 'der Spieler ist nicht online',
-  ['no_money'] = 'you do not have enough money to pay this bill',
-  ['target_no_money'] = 'the player ~r~does not~s~ have enough money to pay the bill!',
+  ['no_money'] = 'Du hast nicht genug Geld, um diese Rechnung zu bezahlen',
+  ['target_no_money'] = 'Der Spieler hat ~r~nicht genug Geld~s~, um die Rechnung zu bezahlen!',
 }
